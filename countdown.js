d9b52569988b7606a7214f7021e1c30d39f841b7
Prem Chandrasekhar <happyprems@gmail.com>
2021-01-21 12:22:28 -0500

    Update

--- countdown.js
+++ countdown.js
@@ -1,28 +1,10 @@
 setInterval(showTime, 1000); 
 function showTime() { 
-    let time = new Date(); 
-    let hour = time.getHours(); 
-    let min = time.getMinutes(); 
-    let sec = time.getSeconds(); 
-    am_pm = "AM"; 
-  
-    if (hour > 12) { 
-        hour -= 12; 
-        am_pm = "PM"; 
-    } 
-    if (hour == 0) { 
-        hr = 12; 
-        am_pm = "AM"; 
-    } 
-  
-    hour = hour < 10 ? "0" + hour : hour; 
-    min = min < 10 ? "0" + min : min; 
-    sec = sec < 10 ? "0" + sec : sec; 
-  
-    let currentTime = hour + ":" 
-            + min + ":" + sec + am_pm; 
+    let currentTime = new Date(); 
+    let graduation = new Date(2022, 5, 20);
+    let diff = graduation - currentTime;
   
     document.getElementById("countdown") 
-            .innerHTML = currentTime; 
+            .innerHTML = diff; 
 } 
 showTime(); 

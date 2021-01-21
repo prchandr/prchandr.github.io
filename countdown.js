84b4f58ee4134765307a2b6a5591e91484ac106d
Prem Chandrasekhar <happyprems@gmail.com>
2021-01-21 12:59:17 -0500

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -4,12 +4,12 @@ function showTime() {
     let graduation = new Date(2022, 5, 20);
     let diff = graduation - currentTime;
 
-    let sec = floor(diff/1000);
-    let day = floor(sec / (60*60*24));
+    let sec = math.floor(diff/1000);
+    let day = math.floor(sec / (60*60*24));
     sec = sec % (60*60*24);
-    let hour = floor(sec / (60*60))
+    let hour = math.floor(sec / (60*60))
     sec = sec % (60*60);
-    let min = floor(sec / 60)
+    let min = math.floor(sec / 60)
     sec = sec % 60
     
     hour = hour < 10 ? "0" + hour : hour; 
@@ -18,7 +18,6 @@ function showTime() {
 
     let timeLeft = day + ":" + hour + ":" + min + ":" + sec;
     console.log(timeLeft)
-    document.getElementById("countdown") 
-            .innerHTML = timeLeft; 
+    document.getElementById("countdown").innerHTML = timeLeft; 
 } 
 showTime(); 
